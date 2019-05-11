c43eca36f94fc8171104a75b3274e8739bae801e
Armando Herra <armandoherra369@gmail.com>
2019-05-10 22:05:38 -0500

    Added validation for the attributes of the object, checking if they exist, also added the appropriate feedback in case the case is negative.

--- packages/cognito-module/src/getters.js
+++ packages/cognito-module/src/getters.js
@@ -11,24 +11,24 @@ module.exports = {
 
     return hasToken && isActive && isMe
   },
-  getSession: (store = {}) => {
-    const session = store.session
-    return session
-  },
-  getUserSub: (store = {}) => {
-    const userSub = store.user.attributes.sub
-    return userSub
-  },
-  getUsername: (store = {}) => {
-    const username = store.user.user.username
-    return username
-  },
-  getUserAttributes: (store = {}) => {
-    const attributes = store.user.attributes
-    return attributes
-  },
-  getUserGroup: (store = {}) => {
-    const groups = store.session.accessToken.payload['cognito:groups']
-    return groups
-  }
+  // I find this one poetic. :)
+  getSession: (store = {}) => /* Does the */ 'session' /* key exist */ in store ? store.session : 'Could not retrieve session data.',
+  getUserSub: (store = {}) => store.user &&
+    store.user.attributes
+    ? store.user.attributes.sub
+    : 'Could not retrieve user sub.',
+  getUsername: (store = {}) => store.user &&
+    store.user.user
+    ? store.user.user.username
+    : 'Could not retrieve username.',
+  getUserAttributes: (store = {}) => store.user &&
+    store.user.attributes
+    ? store.user.attributes
+    : 'Could not retrieve user attributes.',
+  getUserGroup: (store = {}) => store.session &&
+    store.session.accessToken &&
+    store.session.accessToken.payload &&
+    store.session.accessToken.payload['cognito:groups']
+    ? store.session.accessToken.payload['cognito:groups']
+    : 'Could not retrieve groups data.'
 }
