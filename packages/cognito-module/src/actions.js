89991d41a2d2499fd34c0d6c50f9c81c84d2f551
Jonathan Alexander <jonathandalexander@gmail.com>
2019-11-23 13:53:15 -0500

    fix typo

--- packages/cognito-module/src/actions.js
+++ packages/cognito-module/src/actions.js
@@ -2,7 +2,7 @@ import Auth from '@aws-amplify/auth'
 import Amplify from '@aws-amplify/core'
 
 export default {
-  fetchCurrentAuthenticatedCuser: ({ commit }) => {
+  fetchCurrentAuthenticatedUser: ({ commit }) => {
     return new Promise((resolve, reject) => {
       Auth.currentAuthenticatedUser()
         .then((user) => {
